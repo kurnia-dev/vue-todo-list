f5b86419aa22628ea76e078f43ddc989c59432a1
Kurnia-dev <kurniawanviic54@gmail.com>
2023-08-27 02:24:31 +0700

    add scoped style

--- src/components/FormMessage.vue
+++ src/components/FormMessage.vue
@@ -8,3 +8,13 @@ export default {
     props: ['todoList']
 }
 </script>
+
+<style scoped>
+    div {
+        text-align: center;
+        padding: 16px;
+        margin-top: 40px;
+        font-weight: 600;
+        font-size: xxx-large;
+    }
+</style>
