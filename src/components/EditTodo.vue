bfce0a25857385c8e43ec8fa5b11fb674ff448ec
Kurnia-dev <kurniawanviic54@gmail.com>
2023-08-26 23:25:02 +0700

    add script to set caret position to the last character on edit mode

--- src/components/EditTodo.vue
+++ src/components/EditTodo.vue
@@ -13,17 +13,24 @@ export default {
     },
     methods: {
         editTodo(e) {
-            this.isEditing = !this.isEditing 
+            this.isEditing = !this.isEditing
 
             const todoItem = e.target.parentElement
             const todoContent = todoItem.firstElementChild
             const index = todoItem.id
-            todoContent.setAttribute("contentEditable", this.isEditing )
+
+            todoContent.setAttribute("contentEditable", this.isEditing)
             todoContent.focus()
+
             if (!this.isEditing) {
                 // Will be executed if the button text is "Save"
                 const newContent = todoContent.innerText
-                this.$emit('todoEdited', {newContent, index})
+                this.$emit('todoEdited', { newContent, index })
+            } else {
+                // editing mode
+                // set the cursor/caret position to the last character
+                const selection = window.getSelection()
+                selection.setPosition(selection.focusNode, selection.focusNode.length)
             }
         }
     }
