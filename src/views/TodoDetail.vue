102cc6c8ebeb37dbdb81b7911c5b4bd397ccdb74
Kurnia-dev <kurniawanviic54@gmail.com>
2023-08-29 06:24:13 +0700

    add a method to editDetail

--- src/views/TodoDetail.vue
+++ src/views/TodoDetail.vue
@@ -2,35 +2,61 @@
   <div class="todo-detail">
     <button @click="() => this.$router.go(-1)">Kembali</button>
     <h1>{{ todo }}</h1>
-    <p>{{ detail ? detail : "Tidak ada deskripsi." }}</p>
-    <button>{{ detail ? "Ubah Deskripsi" : "Tambahkan Deskripsi" }}</button>
+    <input v-if="isEditing" v-model="detail" type="text" ref="detailInput" />
+    <p v-else ref="detail">{{ detail ? detail : "Tidak ada deskripsi." }}</p>
+    <button v-if="isEditing" @click="() => {
+      isEditing = false
+      saveDetail({ id, detail })
+    }">Simpan Deskripsi</button>
+    <button v-else @click="editDetail">{{ detail ? "Ubah Deskripsi" : "Tambahkan Deskripsi" }}</button>
   </div>
 </template>
 <script>
+import { mapActions } from 'vuex'
 
 export default {
   props: ['id'],
+  data() {
+    return {
+      isEditing: false,
+      todoList: this.$store.state.todoList,
+      detail: this.$store.state.todoList.find(todo => todo.id === this.id).detail
+    }
+  },
   computed: {
-    todoList() {
-      return this.$store.state.todoList
-    },
     todo() {
       return this.todoList.find(todo => todo.id === this.id).todo
-    },
-    detail() {
-      return this.todoList.find(todo => todo.id === this.id).detail
+    }
+  },
+  methods: {
+    ...mapActions(['saveDetail']),
+    editDetail() {
+      this.isEditing = true
+      this.$nextTick(() => {
+        this.$refs.detailInput.focus();
+      });
     }
   }
 }
 </script>
 <style scoped>
-  .todo-detail {
-    display: flex;
-    flex-direction: column;
-    gap: 30px;
-  }
+.todo-detail {
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+}
 
-  .todo-detail button {
-    width: max-content;
-  }
+.todo-detail button {
+  width: max-content;
+}
+
+p,
+input {
+  outline: none;
+  padding: 16px;
+}
+
+input {
+  border: 1px solid #00000017;
+}
 </style>
