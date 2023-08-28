a70aabf03c040577f329eb80eb8c7488f8d7bc87
Kurnia-dev <kurniawanviic54@gmail.com>
2023-08-29 06:34:36 +0700

    import plugin vuex-persistedstate

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue"
 import Vuex from "vuex"
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
@@ -34,5 +35,6 @@ export default new Vuex.Store({
         saveDetail({ commit }, props) {
             commit('detailEdited', props)
         }
-    }
+    }, 
+    plugins: [createPersistedState()]
 })
